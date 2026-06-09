848768355128344231bc653596f55e937ec012ce
Dmitriy Ivkov <divkov@amazon.com>
2026-06-09 15:49:02 -0400

    Exclude build artifacts from Telescope, add unfiltered variants
    Default search ignores build/, env/, .gradle/, brazil-pkg-cache,
    and binary files. <leader>tO and <leader>tG bypass all filters.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,9 +5,15 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
 
+      local ignore = {
+        "node_modules", ".git/", "%.lock",
+        "build/", "env/", ".gradle/", "brazil%-pkg%-cache",
+        "%.class", "%.jar", "%.so", "%.dylib",
+      }
+
       require("telescope").setup({
         defaults = {
-          file_ignore_patterns = { "node_modules", ".git/", "%.lock" },
+          file_ignore_patterns = ignore,
           layout_config = {
             horizontal = {
               preview_width = 0.55,
@@ -21,15 +27,19 @@ return {
           },
         },
         pickers = {
-          find_files = {
-            hidden = true,
-          },
+          find_files = { hidden = true },
         },
       })
 
       vim.keymap.set("n", "<leader>to", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>tO", function()
+        builtin.find_files({ file_ignore_patterns = {} })
+      end, { desc = "Find Files (unfiltered)" })
       vim.keymap.set("n", "<leader>tb", builtin.buffers, { desc = "Open Buffers" })
       vim.keymap.set("n", "<leader>tg", builtin.live_grep, { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>tG", function()
+        builtin.live_grep({ additional_args = function() return { "--no-ignore" } end })
+      end, { desc = "Live Grep (unfiltered)" })
       vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to Definition (Telescope)" })
     end,
   },
